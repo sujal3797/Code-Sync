e755a98073fdae25ffb09c14e6ddd9f6aa7db237
sujal3797 <sujalgupta3797@gmail.com>
2025-08-20 22:28:30 +0530

    Fix server static file serving and routing for React build

--- server.js
+++ server.js
@@ -80,6 +80,13 @@ io.on('connection', (socket) => {
     });
 });
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
